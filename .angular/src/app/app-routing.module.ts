1dac81c86431c766fb8265b357340de8d66adbf3
leonel <leonelpolanco2913@gmail.com>
2023-07-21 08:49:26 -0500

    Profe version
    Profe version

--- .angular/src/app/app-routing.module.ts
+++ .angular/src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./products/products.module').then( m => m.ProductsModule ),
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
